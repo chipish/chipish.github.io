01f3414f0ad29a9d96d4962143513a4d495ad236
chipish <87505372+chipish@users.noreply.github.com>
2021-08-04 23:43:40 -0400

    Add files via upload

--- Project 3/script.js
+++ Project 3/script.js
@@ -13,28 +13,26 @@ const changeMsg = function (msg) {
 //changeMsg('Invalid');
 
 
+while(score>0){
 
+document.querySelector('.check').addEventListener('click', function(){
+    let inputUser1 = document.querySelector('.userInput').value;
 
-document.getElementById('check').addEventListener('click', function(){
-    let inputUser1 = Number(document.getElementById('.userInput').value);
-
-    if (typeof inputUser1 !=="number") {
+    if (typeof(inputUser1) !=="number") {
         changeMsg('Please enter a valid number!');
-        //document.querySelector('.p1').textContect= 'Please enter a valid number!'
     }
-    else if (inputUser1 < 1 || inputUser1 > 100 ){
+
+    else if ((inputUser1 < 1 )|| (inputUser1 > 100)){
         changeMsg('Please enter a number between 1 and 100!');
-
-
     }
 
     else if(inputUser1 === secretNumber){
         changeMsg('Congrats! You got the number');
 
-        document.querySelector('page').style.background = 'powderblue';
-        document.querySelector('guess-container').style.background = 'powerblue'; 
-        document.querySelector('guessbox').value = secretNumber; 
-        cument.querySelector('page').style.background = 'linear-gradient (to top right, blue, red)';
+        document.querySelector('.page').style.background = 'powderblue';
+        document.querySelector('.guess-container').style.background = 'powerblue'; 
+        document.querySelector('.guessbox').value = secretNumber; 
+        document.querySelector('.page').style.background = 'linear-gradient (to top right, blue, red)';
     }
 });
 
@@ -42,24 +40,24 @@ if(score > highscore){
     highscore = score;
 }
 else if (inputUser1 !== secretNumber){
-    if (score > 0){ changeMsg((inputUser1 > secretNumber) ? 'Too High' : 'Too Low')}
+    if (score > 0) { changeMsg((inputUser1 > secretNumber) ? 'Too High' : 'Too Low')}
 
 }
 
-else if(score === 0){
+else {
     changeMsg('You lost the game!')
-    //document.querySelector('check').disabled = true;
+    document.querySelector('.check').disabled = true;
     break;
 }
 
     score--;
 
-    document.querySelector('score').value = score;
-    document.querySelector('highscore').value = highScore;
+    document.querySelector('.score').value = score;
+    document.querySelector('.highscore').value = highScore;
 
+}
 
-
-document.getElementById('play').addEventListener('click', function () {
+document.querySelector('.play').addEventListener('click', function () {
 
     let score = 10;
     let secretNumber = Math.trunc(Math.random() * 100) + 1;
