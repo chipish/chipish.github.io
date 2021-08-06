06573bde77912c1070f621b4081a18146c2f0e1a
chipish <87505372+chipish@users.noreply.github.com>
2021-08-06 08:09:37 -0400

    Add files via upload

--- Project 3/script.js
+++ Project 3/script.js
@@ -4,43 +4,61 @@ console.log(secretNumber);
 let score = 10;
 let highScore = 0;
 
-const textElement = document.querySelector('.p1');
-textElement.style.color = 'red';
+let message = document.querySelector('.p1');
+message.style.color = 'red';
 
-const changeMsg = function (msg) {
-    textElement.textContent = msg;
+let changeMsg = function (msg) {
+    message.textContent = msg;
 };
 
-const clickBtn = document.querySelector('.check');
+let clickBtn = document.querySelector('.check');
+let inputUser1 = document.querySelector('.userInput');
 
-while(score >0){
+if (score < 1){
+    changeMsg('Game Over! You Lost!');
+    document.querySelector('.check').disabled = true;
+
+}
 
 clickBtn.addEventListener('click', function(){
-    let inputUser1 = document.querySelector('.userInput').value;
+    
 
-    if (typeof(inputUser1) !=="number") {
+    if (inputUser1 == '') {
         changeMsg('Please enter a valid number!');
     }
 
     else if ((inputUser1 < 1 )|| (inputUser1 > 100)){
         changeMsg('Please enter a number between 1 and 100!');
+        
     }
 
-    else if (inputUser1 !== secretNumber){
-        changeMsg((inputUser1 > secretNumber) ? 'Too High' : 'Too Low');
+    else if (inputUser1 > secretNumber){
+        changeMsg('Too High');
+        score--;
+        document.querySelector('.score').innerHTML = score;
+
+    }
+
+    else if (inputUser1 < secretNumber){
+        changeMsg('Too Low');
+        score--;
+        document.querySelector('.score').innerHTML = score;
+
     }
     else if(inputUser1 === secretNumber){
         changeMsg('Congrats! You got the number');
 
         document.querySelector('.page').style.background = 'powderblue';
-        document.querySelector('.guess-container').style.background = 'powerblue'; 
+        document.querySelector('.guess-container').style.background = 'red'; 
         document.querySelector('.guessbox').value = secretNumber; 
         document.querySelector('.page').style.background = 'linear-gradient (to top right, blue, red)';
+
+        document.querySelector('.check').disabled = true;
+
     }
 
     else {
-        changeMsg('You lost the game!')
-        document.querySelector('.check').disabled = true;
+        changeMsg('Please enter a valid number!');
     }
 });
 
@@ -48,17 +66,16 @@ if(score > highScore){
     highscore = score;
 }
 
-    score--;
 
-    document.querySelector('.score').value = score;
-    document.querySelector('.highscore').value = highScore;
+    document.querySelector('.score').innerHTML = score;
+    document.querySelector('.highscore').innerHTML = highScore;
 
-}
+
 
 document.querySelector('.play').addEventListener('click', function () {
 
-    let score = 10;
-    let secretNumber = Math.trunc(Math.random() * 100) + 1;
+    score = 10;
+    secretNumber = Math.trunc(Math.random() * 100) + 1;
     changeMsg('Lets start Guessing...');
 
 
